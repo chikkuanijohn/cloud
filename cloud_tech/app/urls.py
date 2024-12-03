2217821651e9a8943864f7d0c963f11a7343725d
chikkuanijohn <chikkuanijohn1@gmail.com>
2024-12-03 21:56:57 +0530

    new file

---
+++ cloud_tech/app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns=[
+    path('',views.cloud_tech_login),
+    path('register',views.register),
+    path('home',views.home),
+]
---
+++ cloud_tech/app/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
+from .models import *
+from django.contrib.auth.models import User
+
+# Create your views here.
+def cloud_tech_login(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        password=req.POST['password']
+        data=authenticate(username=uname,password=password)
+        if data:
+            login(req,data)
+            return redirect(home)
+        else:
+            messages.warning(req,'invalid username or password') 
+            return redirect(cloud_tech_login)     
+    else:     
+        return render(req,'login.html')
+    
+def cloud_tech_logout(req):
+    logout(req)
+    return redirect(cloud_tech_login)    
+
+def register(req):
+    if req.method=='POST':
+        uname=req.POST['uname']
+        email=req.POST['email']
+        pswd=req.POST['pswd']
+        try:
+            data=User.objects.create_user(first_name=uname,email=email,username=email,password=pswd)
+            data.save()
+        except:
+            messages.warning(req,'invalid username or password')
+            return redirect(register)   
+        return redirect(cloud_tech_login) 
+    else:
+        return render(req,'register.html')    
+    
+def home(req):
+    return render(req,'home.html')
